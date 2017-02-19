f0581b7973ef817b93f95e7ce9fd9c871e2f47c1
m4cx <macgyverx83@gmail.com>
2017-02-19 22:04:44 +0100

    Add a Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -15,12 +16,20 @@ import { RouterModule } from '@angular/router';
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
+    DashboardComponent,
   ],
   imports: [
     RouterModule.forRoot([
       {
         path: 'heroes',
         component: HeroesComponent
+      }, {
+        path: 'dashboard',
+        component: DashboardComponent
+      }, {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
       }
     ]),
     BrowserModule,
