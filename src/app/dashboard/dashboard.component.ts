ec49a04ff10b245c33179110eeb2773c617135c0
m4cx <macgyverx83@gmail.com>
2017-02-19 22:07:55 +0100

    Dashboard Top Heroes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,5 @@
+import { Hero } from './../hero';
+import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
 
-  ngOnInit() {
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
   }
-
 }
