6b8a743da199bea30ea7440e5faffd635fd097e3
m4cx <macgyverx83@gmail.com>
2017-02-24 10:34:41 +0100

    Heroes and HTTP: Get Hero by Id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Hero } from './hero';
 
-import { Injectable }    from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
@@ -8,26 +8,29 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class HeroService {
 
- private heroesUrl = 'api/heroes';  // URL to web api
+  private heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(private http: Http) { }
 
   getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl)
-               .toPromise()
-               .then(response => response.json().data as Hero[])
-               .catch(this.handleError);
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  getHero(id: number): Promise<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
       setTimeout(() => resolve(this.getHeroes()), 2000);
     });
-  }
-
-  getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
   }
 
   private handleError(error: any): Promise<any> {
